b4be993d9aa85e35936ebdf2e889eb7dddadb781
Santiago Ospina <sospina@Santiagos-MacBook-Pro.local>
2020-05-06 11:32:31 -0400

    Re-wrote implementation to be easier to understand + started while loop for main game

--- game.py
+++ game.py
@@ -1,8 +1,7 @@
+from player import Player
+from card import Card
 import random
-
 import constants
-import player
-import card
 
 class Game:
 	def __init__(self):
@@ -13,7 +12,7 @@ class Game:
 		self.cards = []
 		for suit in constants.CARD_SUITS:
 			for val in range(1, constants.MAX_CARD_VALUE + 1):
-				self.cards.append(card.Card(val, suit))
+				self.cards.append(Card(val, suit))
 		random.shuffle(self.cards)
 
 	def add_player(self, player):
@@ -22,40 +21,38 @@ class Game:
 	def distribute_cards(self):
 		num_of_players = len(self.players)
 		i = 0
-		for c in self.cards:
+		for card in self.cards:
 			turn = i % num_of_players
-			self.players[turn].add_card(c)
+			self.players[turn].add_card(card)
 			i += 1
+
+	def play_game(self):
+		starting_player_index = self.start_round()
+		current_player_index = starting_player_index
+		while (not all(len(player.cards) == 0 for player in self.players)):
+			current_player_index = (current_player_index + 1) % len(self.players)
+			print(current_player_index)
+			if current_player_index == 0:
+				break
 
 	def start_round(self):
 		self.top_of_deck = None
-		three_of_clubs = card.Card(3, constants.CLUBS)
-		starting_player = None
-		for p in self.players:
-			if three_of_clubs in p.cards:
-				starting_player = p
-		if starting_player is None: raise NameError("No 3 of clubs found")
+		three_of_clubs = Card(3, constants.CLUBS)
+		try: starting_player_index, starting_player = next((i, player) for i, player in enumerate(self.players) if three_of_clubs in player.cards)
+		except: raise NameError("No 3 of clubs found")
+		print("The starting player is " + starting_player.name + " at index " + str(starting_player_index))
+		return starting_player_index
 
-		print("The starting player is " + starting_player.name)
-
-		#Wait for player to pick card to play
-		#await starting_player.use_card()
-		#self.play_card(starting_player, card)
-
-
-	def play_card(self, player, c):
-		if self.top_of_deck is None or self.top_of_deck.val < c.val:
-			self.top_of_deck = c
-
-
-
+	def play_card(self, player, card):
+		if self.top_of_deck is None or self.top_of_deck.val < card.val:
+			self.top_of_deck = card
 
 
 g = Game()
-g.add_player(player.Player("a"))
-g.add_player(player.Player("b"))
-g.add_player(player.Player("c"))
-g.add_player(player.Player("d"))
+g.add_player(Player("a"))
+g.add_player(Player("b"))
+g.add_player(Player("c"))
+g.add_player(Player("d"))
 
 g.distribute_cards()
-g.start_round()
+g.play_game()
