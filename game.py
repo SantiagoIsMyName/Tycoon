f6f28702895bc868d497ed38e6f5f296396c2891
Santiago Ospina <sospina@Santiagos-MacBook-Pro.local>
2020-05-06 13:52:34 -0400

    Added 8 stop rule functionality

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ import constants
 THREE_OF_CLUBS = Card(3, constants.CLUBS)
 
 def create_shuffled_deck():
-	cards = [Card(val, suit) for val, suit in zip(constants.NORMAL_ORDERING, constants.CARD_SUITS)]
+	cards = [Card(val, suit) for val in constants.NORMAL_ORDERING for suit in constants.CARD_SUITS]
 	random.shuffle(cards)
 	return cards
 
@@ -29,7 +29,7 @@ class Game:
 		num_of_players = len(self.players)
 		i = 0
 
-		#Deal cards by cycling through each player and giving them a card until are cards are dealed
+		# Deal cards by cycling through each player and giving them a card until are cards are dealed
 		for card in cards:
 			turn = i % num_of_players
 			self.players[turn].add_card(card)
@@ -38,26 +38,38 @@ class Game:
 	def play_game(self):
 		self.distribute_cards()
 
-		top_of_deck = None
+		top_of_pile = None
 		final_rankings = []
 		current_player_index = starting_player_index(self.players)
 		last_played_index = -1
+
 		while (len(self.players) > 1):
 			current_player = self.players[current_player_index]
 
+			# If it's the last played person's turn again (as in everyone else passes), the pile gets cleared
 			if last_played_index == current_player_index:
-				top_of_deck = None
+				top_of_pile = None
 
-			card_to_play = current_player.play_card(top_of_deck)
+			card_to_play = current_player.play_card(top_of_pile)
 
+			# If the player decided to play a card, it replaces the top card
 			if card_to_play:
-				top_of_deck = card_to_play
+				top_of_pile = card_to_play
 				last_played_index = current_player_index
 
-			if len(current_player.cards) == 0:
-				final_rankings.append(current_player)
-				self.players.remove(current_player)
-				last_played_index = last_played_index % len(self.players)
+				# Eight stop: Player who plays an 8 clears the pile, starts a new one
+				if card_to_play.val == 8:
+					top_of_pile = None
+
+				# If the current player has just played their last card, they are added to the rankings and no longer play
+				if len(current_player.cards) == 0:
+					final_rankings.append(current_player)
+					self.players.remove(current_player)
+					last_played_index = last_played_index % len(self.players)
+
+				if top_of_pile is None:
+					continue
+
 
 			current_player_index = (current_player_index + 1) % len(self.players)
 
--- player.py
+++ player.py
@@ -9,16 +9,16 @@ class Player:
 		self.cards.append(card)
 
 	def play_card(self, current_card):
-		#If it's an empty pile, play any card you want (for now, smallest value)
+		# If it's an empty pile, play any card you want (for now, smallest value)
 		if current_card is None: 
 			card_to_play = min(self.cards, key = lambda card: NORMAL_COMPARATOR[card.val])
 
-		#If pile isn't empty, play any card larger than the current top card (for now, smallest value that satisfies this)
+		# If pile isn't empty, play any card larger than the current top card (for now, smallest value that satisfies this)
 		else: 
 			card_to_play = next((card for card in self.cards if NORMAL_COMPARATOR[card.val] > NORMAL_COMPARATOR[current_card.val]), None)
 
-		#If you have a card to play, remove it from your hand
+		# If you have a card to play, remove it from your hand
 		if card_to_play:
 			self.cards.remove(card_to_play)
-			
+
 		return card_to_play
