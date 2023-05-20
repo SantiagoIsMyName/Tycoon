0a90c7dc64b322a34aef47d3b7a7c61520765099
Santiago Ospina <santiom97@yahoo.com>
2023-05-20 14:40:51 -0400

    User now inputs player names prior to starting game

--- game.py
+++ game.py
@@ -221,13 +221,12 @@ class Game:
 		return self.leaderboard
 
 
-g = Game()
-g.add_player(Player("A"))
-g.add_player(Player("B"))
-g.add_player(Player("C"))
-g.add_player(Player("D"))
-
 rounds_requested = int(input("How many rounds would you like to play? \n"))
+print("____________________")
+g = Game()
+for i in range(1, 5):
+	player_name = input(f"What is Player {i}'s name? \n")
+	g.add_player(Player(player_name))
 print("____________________")
 rankings = g.play_game_with_rounds(rounds_requested)
 
